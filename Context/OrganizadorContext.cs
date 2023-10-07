d09bd54f8c0672f10172a69c87b480df7a7122b7
jessicacosta07 <jessikacostajf@gmail.com>
2023-10-07 01:29:38 -0300

    implementação inicial

---
+++ Context/OrganizadorContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tasks_system.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace tasks_system.Context
+{
+    public class OrganizadorContext : DbContext
+    {
+        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
---
+++ Models/EnumStatusTarefa.cs
@@ -0,0 +1,8 @@
+namespace tasks_system.Models
+{
+    public enum EnumStatusTarefa
+    {
+        Pendente,
+        Finalizado
+    }
+}
